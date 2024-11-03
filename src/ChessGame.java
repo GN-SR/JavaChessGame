43b2b33918ba7fabad256a456466e49bd6c919ed
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-03 13:34:53 +0530

    Developing game logic

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -17,4 +17,78 @@ public class ChessGame {
         }
         return false;
     }
+
+    public boolean isInCheck(PieceColor kingColor) {
+        Position kingPosition = findKingPosition(kingColor);
+        for (int row = 0; row < board.getBoard().length; row++){
+            for(int col = 0; col < board.getBoard()[row].length; col++){
+                Piece piece = board.getPiece(row, col);
+                if(piece != null && piece.getColor() != kingColor){
+                    if(piece.isValidMove(kingPosition, board.getBoard())){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private Position findKingPosition(PieceColor color) {
+        for(int row = 0; row < board.getBoard().length; row++){
+            for(int col = 0; col <board.getBoard()[row].length; col++){
+                Piece piece = board.getPiece(row, col);
+                if( piece instanceof King &&  piece.getColor() == color){
+                    return new Position(row, col);
+                }
+            }
+        }
+        throw new RuntimeException("King not found, which should never happen.");
+    }
+
+    public boolean isCheckmate(PieceColor kingColor) {
+        if(!isInCheck(kingColor)) {
+            return false;
+        }
+        Position kingPosition = findKingPosition(kingColor);
+        King king = (King) board.getPiece(kingPosition.getRow(), kingPosition.getColumn());
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            for(int colOffset = -1; colOffset <=1; colOffset++){
+                if(rowOffset == 0 && colOffset == 0){
+                    continue;
+                }
+                Position newPosition = new Position(kingPosition.getRow() + rowOffset, kingPosition.getColumn() + colOffset);
+                if(isPositionOnBoard(newPosition) && king.isValidMove(newPosition, board.getBoard()) && !wouldBeInCheckAfterMove(kingColor, kingPosition, newPosition)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isPositionOnBoard(Position position) {
+        return position.getRow() >= 0 && position.getRow() < board.getBoard().length && position.getColumn() >= 0 && position.getColumn() < board.getBoard()[0].length;
+    }
+    private boolean wouldBeInCheckAfterMove(PieceColor kingColor, Position from, Position to){
+        Piece temp = board.getPiece(to.getRow(), to.getColumn());
+        board.setPiece(to.getRow(), to.getColumn(), board.getPiece(from.getRow(), from.getColumn()));
+        board.setPiece(from.getRow(), from.getColumn(), null);
+
+        boolean inCheck = isInCheck(kingColor);
+
+        board.setPiece(from.getRow(), from.getColumn(), board.getPiece(to.getRow(), to.getColumn()));
+        board.setPiece(to.getRow(), to.getColumn(), temp);
+        return inCheck;
+    }
+
+    public Piece[][] getBoard() {
+        return board;
+    }
+    public Piece getPiece(int row, int column) {
+        return board[row][column];
+    }
+    public void setPiece(int row, int column, Piece piece) {
+        board[row][column] = piece;
+        if(piece != null) {
+            piece.setPosition(new Position(row, column));
+        }
+    }
 }
